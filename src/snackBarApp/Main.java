721ac2313ba5ac01e1903eb69ddea379ceab5a48
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-01 12:56:10 -0700

    done with initial set up of files and have run the base program.

---
+++ src/snackBarApp/Main.java
@@ -0,0 +1,13 @@
+package snackBarApp;
+
+public class Main
+{
+	private static void vendForCustomers()
+	{
+		System.out.println("Greetings hungry office worker!");
+	}
+	public static void main(String[] args)
+	{
+		vendForCustomers();
+	}
+}
